0603979fd33860b4f50087562589b9ae95c264ff
monknike <mayanknaik2000@gmail.com>
2021-11-11 18:29:26 +0000

    Tiny guard change-

--- RDVFSharp-patch-2/RDVFSharp/Commands/Ingame/Guard.cs
+++ RDVFSharp-patch-2/RDVFSharp/Commands/Ingame/Guard.cs
@@ -1,7 +1,24 @@
 ﻿using FChatSharpLib.Entities.Plugin.Commands;
 using RDVFSharp.Errors;
+using System.Collections.Generic;
+
 
 namespace RDVFSharp.Commands
 {
-    public class Guard : Action {}
+    public class Guard : Action
+
+    {
+        public override void ExecuteCommand(string character, IEnumerable<string> args, string channel)
+    {
+        if (Plugin.CurrentTeamBattlefield.IsActive)
+        {
+            base.ExecuteCommand(character, args, channel);
+        }
+
+        if (Plugin.CurrentBattlefield.IsActive)
+        {
+            Plugin.FChatClient.SendMessageInChannel("This cannot be used in 1v1 fights.", Plugin.Channel);
+        }
+    }
+}
 }
